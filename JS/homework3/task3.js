02919f3c4ae234eae9bf170fb69f66fe294a0cdb
Vladimir Shytsko <shytsko.v@gmail.com>
2024-07-20 13:56:47 +0300

    Fix task3

--- JS/homework3/task3.js
+++ JS/homework3/task3.js
@@ -4,11 +4,15 @@ const max2 = (x, y) => x > y ? x : y;
 
 const max3 = (x, y, z) => max2(max2(x, y), z);
 
-const num1 = prompt("Первое число");
-const num2 = prompt("Второе число");
-const num3 = prompt("Третье число");
+const num1 = +prompt("Первое число");
+const num2 = +prompt("Второе число");
+const num3 = +prompt("Третье число");
 
-console.log(max3(num1, num2, num3));
-console.log(max3(num2, num1, num3));
-console.log(max3(num2, num3, num1));
-console.log(max3(num2, num1, num3));
+if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+    console.log("Одно из значений не является числом");
+} else {
+    console.log(max3(num1, num2, num3));
+    console.log(max3(num2, num1, num3));
+    console.log(max3(num2, num3, num1));
+    console.log(max3(num2, num1, num3));
+}
