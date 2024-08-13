9ebc4478c2ca53422b795a191434f49a1c8df4f8
Vladimir Shytsko <shytsko.v@gmail.com>
2024-08-13 11:21:05 +0300

    Add homework 10

--- JS/homework10/data.js
+++ JS/homework10/data.js
@@ -1,92 +1,63 @@
-const dataInfo = `
-[
-      {
-        "id": 25308,
-        "title": "SpaceX aces sunrise Falcon 9 launch with 23 Starlink satellites",
-        "url": "https://www.teslarati.com/spacex-aces-sunrise-falcon-9-launch-with-23-starlink-satellites/",
-        "image_url": "https://www.teslarati.com/wp-content/uploads/2024/08/spacex-starlink-sunrise.jpeg",
-        "news_site": "Teslarati",
-        "summary": "SpaceX aced an early morning launch complimented by a beautiful sunrise on Monday, sending a Falcon 9 equipped with 23 Starlink satellites...",
-        "published_at": "2024-08-12T19:27:08Z",
-        "updated_at": "2024-08-12T19:27:12.822041Z",
-        "featured": false,
-        "launches": [],
-        "events": []
-      },
-      {
-        "id": 25307,
-        "title": "Station Science Top News: August 9, 2024",
-        "url": "https://www.nasa.gov/centers-and-facilities/johnson/station-science-top-news-august-9-2024/",
-        "image_url": "https://www.nasa.gov/wp-content/uploads/2024/08/iss060e045091.jpg?w=2048",
-        "news_site": "NASA",
-        "summary": "Researchers tested a treatment on cartilage and bone tissue cultures subjected to compressive impact injury and found differences in the metabolites and proteins released by cells in space and on Earth along with partial improvement in both gravity conditions. The findings suggest the treatment is safe and could help ensure the health of crew members on future [&#8230;]",
-        "published_at": "2024-08-12T18:08:06Z",
-        "updated_at": "2024-08-12T18:17:25.894925Z",
-        "featured": false,
-        "launches": [],
-        "events": []
-      },
-      {
-        "id": 25306,
-        "title": "Webb Sees Gassy Baby Stars",
-        "url": "https://www.nasa.gov/image-article/webb-sees-gassy-baby-stars/",
-        "image_url": "https://www.nasa.gov/wp-content/uploads/2024/08/53804051336-3a87b00dce-o.png?w=2000",
-        "news_site": "NASA",
-        "summary": "NASA’s James Webb Space Telescope has captured a phenomenon for the very first time. The bright red streaks at top left of this June 20, 2024, image are aligned protostar outflows – jets of gas from newborn stars that all slant in the same direction. This image supports astronomers’ assumption that as clouds collapse to [&#8230;]",
-        "published_at": "2024-08-12T17:57:10Z",
-        "updated_at": "2024-08-12T18:07:09.056820Z",
-        "featured": false,
-        "launches": [],
-        "events": []
-      },
-      {
-        "id": 25304,
-        "title": "Sols 4270-4272: Sample for SAM",
-        "url": "https://science.nasa.gov/blogs/sols-4270-4272-sample-for-sam/",
-        "image_url": "https://science.nasa.gov/wp-content/uploads/2024/08/https-mars-nasa-gov-msl-raw-images-msss-04253-mhli-4253mh0001930001502657r00-dxxx.jpg?w=1584",
-        "news_site": "NASA",
-        "summary": "Earth planning date: Friday, Aug. 9, 2024 The focus for this three-sol weekend plan is delivering a portion of the Kings Canyon drill sample to SAM for Evolved Gas Analysis (EGA), following on from a successful CheMin analysis. The CheMin and SAM analyses, coupled with APXS and ChemCam analyses, will tell us about the composition […]",
-        "published_at": "2024-08-12T15:45:37Z",
-        "updated_at": "2024-08-12T16:07:25.897251Z",
-        "featured": false,
-        "launches": [],
-        "events": []
-      },
-      {
-        "id": 25303,
-        "title": "Moon to Mars Part I: Forging the Path",
-        "url": "https://www.spacescout.info/2024/08/moon-to-mars-part-i-forging-the-path/",
-        "image_url": "https://i0.wp.com/www.spacescout.info/wp-content/uploads/2024/01/orionmoonearth.jpeg",
-        "news_site": "Space Scout",
-        "summary": "At its core, the Artemis Program represents a new way of thinking in space exploration. NASA’s Moon to Mars objectives incorporate the Artemis Program as part of the roadmap to the Red Planet.",
-        "published_at": "2024-08-12T15:36:31Z",
-        "updated_at": "2024-08-12T15:38:06.588899Z",
-        "featured": false,
-        "launches": [],
-        "events": []
-      },
-      {
-        "id": 25302,
-        "title": "NASA to Provide Coverage of Progress 89 Launch, Space Station Docking",
-        "url": "https://www.nasa.gov/news-release/nasa-to-provide-coverage-of-progress-89-launch-space-station-docking/",
-        "image_url": "https://www.nasa.gov/wp-content/uploads/2024/08/53376035451-a549d80ef0-k.jpg?w=2048",
-        "news_site": "NASA",
-        "summary": "NASA will provide live launch and docking coverage of a Roscosmos cargo spacecraft delivering nearly three tons of food, fuel, and supplies to the Expedition 71 crew aboard the International Space Station. The unpiloted Progress 89 spacecraft is scheduled to launch at 11:20 p.m. EDT, Wednesday, Aug. 14 (8:20 a.m. Baikonur time, Thursday, Aug. 15), [&#8230;]",
-        "published_at": "2024-08-12T15:29:09Z",
-        "updated_at": "2024-08-12T16:02:22.419694Z",
-        "featured": false,
-        "launches": [
-          {
-            "launch_id": "fa14deb0-bd6f-4318-922e-4cbe11773e54",
-            "provider": "Launch Library 2"
-          }
-        ],
-        "events": [
-          {
-            "event_id": 1027,
-            "provider": "Launch Library 2"
-          }
-        ]
-      }
-    ]
-`
+// https://api.spaceflightnewsapi.net/v4/articles/?limit=4
+
+const newsRawData = `
+{
+  "count": 22052,
+  "next": "https://api.spaceflightnewsapi.net/v4/articles/?limit=4&offset=4",
+  "previous": null,
+  "results": [
+    {
+      "id": 25313,
+      "title": "SpaceX Denies Violating Environmental Regulations at Starbase",
+      "url": "https://spacepolicyonline.com/news/spacex-denies-violating-environmental-regulations-at-starbase/",
+      "image_url": "https://spacepolicyonline.com/wp-content/uploads/2023/11/Starship-prior-to-IFT2-2-222x300.jpg",
+      "news_site": "SpacePolicyOnline.com",
+      "summary": "SpaceX is denying a report by CNBC today that it is violating environmental regulations at its Starbase facility in Boca Chica, Texas. Citing notices from the Texas environmental quality commission […]",
+      "published_at": "2024-08-12T23:22:13Z",
+      "updated_at": "2024-08-12T23:27:14.045744Z",
+      "featured": false,
+      "launches": [],
+      "events": []
+    },
+    {
+      "id": 25312,
+      "title": "SpaceX to launch privately-financed international crew of four around Earth’s poles",
+      "url": "https://spaceflightnow.com/2024/08/12/spacex-to-launch-privately-financed-international-crew-of-four-around-earths-poles/",
+      "image_url": "http://spaceflightnow.com/wp-content/uploads/2024/08/FRAM2-Crew.jpg",
+      "news_site": "Spaceflight Now",
+      "summary": "A blockchain entrepreneur, a cinematographer, a polar adventurer and a robotics researcher plan to fly around Earth's poles aboard a SpaceX Crew Dragon capsule by end of year, becoming the first humans to observe the ice caps and extreme polar environments from orbit, SpaceX announced Monday.",
+      "published_at": "2024-08-12T23:11:09Z",
+      "updated_at": "2024-08-12T23:16:48.446226Z",
+      "featured": false,
+      "launches": [],
+      "events": []
+    },
+    {
+      "id": 25311,
+      "title": "Launch Roundup: Progress MS-28, Indian SSLV, and Falcon 9 flights scheduled",
+      "url": "https://www.nasaspaceflight.com/2024/08/launch-roundup-081224/",
+      "image_url": "https://www.nasaspaceflight.com/wp-content/uploads/2024/08/IMG_4381.jpeg",
+      "news_site": "NASASpaceflight",
+      "summary": "The second full week of August is another busy one, with four flights scheduled including the final certification flight of a new launch vehicle from India. The Falcon 9 and Soyuz rockets are also scheduled to fly this week with one mission to the International Space Station and a pair of non-Starlink flights from both coasts of the United States.",
+      "published_at": "2024-08-12T23:02:54Z",
+      "updated_at": "2024-08-12T23:06:57.284511Z",
+      "featured": false,
+      "launches": [],
+      "events": []
+    },
+    {
+      "id": 25310,
+      "title": "SpaceX announces first human mission to ever fly over the planet’s poles",
+      "url": "https://arstechnica.com/space/2024/08/spacex-announces-first-human-mission-to-ever-fly-over-the-planets-poles/",
+      "image_url": "https://cdn.arstechnica.net/wp-content/uploads/2024/08/fram2.jpg",
+      "news_site": "Arstechnica",
+      "summary": "The highest inclination flight by a human spacecraft to date is 65.1 degrees.",
+      "published_at": "2024-08-12T22:45:05Z",
+      "updated_at": "2024-08-12T22:57:02.203252Z",
+      "featured": false,
+      "launches": [],
+      "events": []
+    }
+  ]
+}
+`;
--- JS/homework10/script.js
+++ JS/homework10/script.js
@@ -1,3 +1,45 @@
-const data = JSON.parse(dataInfo)
+const newsData = JSON.parse(newsRawData);
 
-console.log(data);
+function createNewsCard(news) {
+  const card = document.createElement("article");
+  card.classList.add("news");
+  card.innerHTML = `
+      <h2 class="news__title"></h2>
+      <p class="news__datetime-publiched">Опубликовано: <time datetime=""></time></p>
+      <p class="news__origin">Источник: <cite></cite></p>
+      <img class="news__image" src="" alt="news image"/>
+      <p class="news__summary"></p>
+      <a class="news__link" href="" target="_blank">Читать ...</a>
+    `;
+
+  card.querySelector(".news__title").textContent = news.title;
+  card.querySelector(".news__origin cite").textContent = news.news_site;
+  card.querySelector(".news__image").src = news.image_url;
+  card.querySelector(".news__summary").textContent = news.summary;
+  card.querySelector(".news__link").href = news.url;
+
+  const datetimePubliched = card.querySelector(
+    ".news__datetime-publiched time"
+  );
+  datetimePubliched.setAttribute("datetime", news.published_at);
+  const dt = new Date(Date.parse(news.published_at));
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+    timezone: "UTC",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  datetimePubliched.textContent = dt.toLocaleString("ru", options);
+
+  return card;
+}
+
+const newsContent = document.querySelector(".news-content");
+
+newsData.results.forEach((news) => {
+  card = createNewsCard(news);
+  newsContent.append(card);
+});
