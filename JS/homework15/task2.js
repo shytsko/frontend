becef432e05fa719011bc6547abdab4d9ccb0e3a
Vladimir Shytsko <shytsko.v@gmail.com>
2024-09-02 23:20:00 +0300

    Add homework 15

---
+++ JS/homework15/task2.js
@@ -0,0 +1,44 @@
+// Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+// Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+// Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Order {
+    constructor(orderNumber) {
+        this.orderNumber = orderNumber;
+        this.products = [];
+    }
+
+    addProduct(product) {
+        if (product instanceof Product) {
+            this.products.push(product);
+        } else {
+            console.log("Not product");
+        }
+    }
+
+    getTotalPrice() {
+        return this.products.reduce((sum, item) => sum + item.price, 0);
+    }
+}
+
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100);
+order.addProduct(product2);
+
+const product3 = {};
+order.addProduct(product3);
+
+console.log(order.getTotalPrice()); // Вывод: 600
