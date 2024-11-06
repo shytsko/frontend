bd43214ab525c95ef79323852951df4651a4f8a1
Uladzimir Shytsko <92379291+shytsko@users.noreply.github.com>
2024-11-06 08:26:14 +0300

    Update App.js fix component name

--- react/homework4/src/App.js
+++ react/homework4/src/App.js
@@ -3,14 +3,14 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import HomePage from "./routes/HomePage";
 import AboutPage from "./routes/AboutPage";
 import NavBar from "./components/NavBar";
-import NotFoundtPage from "./routes/NotFoundtPage";
+import NotFoundPage from "./routes/NotFoundPage";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="*" element={<NotFoundtPage />} />
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
