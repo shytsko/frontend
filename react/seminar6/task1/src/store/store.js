6bf646a7cea6350b0c7add9804415f29c8654f58
Vladimir Shytsko <shytsko.v@gmail.com>
2024-11-11 21:50:21 +0300

    Seminar 7 task 1

--- react/seminar6/task1/src/store/store.js
+++ react/seminar6/task1/src/store/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import todoListReducer from './reducers/todoListReducer'
 
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    console.log('Действие:', action);
+    console.log('Состояние до выполнения:', store.getState());
+    let result = next(action);
+    console.log('Состояние после выполнения:', store.getState());
+    return result;
+};
+
 export default configureStore({
     reducer: {
-        todoList: todoListReducer,
+        todoList: todoListReducer
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(loggerMiddleware),
 })
