573aeda377a91f16978a0b13aaf216b80b7f1886
Vladimir Shytsko <shytsko.v@gmail.com>
2024-11-20 00:31:40 +0300

    add router

--- react/homework8/src/components/header/Menu.js
+++ react/homework8/src/components/header/Menu.js
@@ -1,6 +1,5 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
 import MenuItems from "./MenuItems";
 
 const menuInit = [
--- react/homework8/src/index.js
+++ react/homework8/src/index.js
@@ -6,12 +6,25 @@ import './styles/menu.scss'
 import './styles/advantages.scss'
 import './styles/subscribe.scss'
 import './styles/footer.scss'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
